f6445097bd8c51e9b02f3644e9b825361d27d9a1
Henry <henryjeremyas@gmail.com>
2023-08-21 10:44:43 -0600

    Codificación de controladores

--- CatalogoEmpresa.AppWeb/src/java/catalogoempresa/appweb/controllers/HomeServlet.java
+++ CatalogoEmpresa.AppWeb/src/java/catalogoempresa/appweb/controllers/HomeServlet.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package catalogoempresa.appweb.controllers;
 
-/**
- *
- * @author Henry
- */
-public class HomeServlet {
-    
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HomeServlet", urlPatterns = {"/Home"})
+public class HomeServlet extends HttpServlet {
+
+    private void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGetRequestIndex(request, response);
+    }
 }
